9c81fdce598b7b60bb0aa4b327cf6b4400dd441f
Eliska Veisova <panvicka@gmail.com>
2023-01-19 09:12:54 +0100

    extract schedule id to env variable

--- src/api/OpsGenieCall.ts
+++ src/api/OpsGenieCall.ts
@@ -3,7 +3,7 @@ import { env } from '$env/dynamic/private';
 
 const opsGenieRequest = async () => {
 	const response = await fetch(
-		`https://api.opsgenie.com/v2/schedules/b2700d37-4eb7-4f4e-9019-dc2eecef0b09/on-calls`,
+		`https://api.opsgenie.com/v2/schedules/${env.PRIVATE_OCD_SCHEDULE_ID}/on-calls`,
 		{
 			headers: {
 				Authorization: `GenieKey ${env.PRIVATE_OCD_API_KEY}`,
